12ed8e1e23ac208bc01d7c989b5eea66ff31c174
Andrew <andrewaoidh@gmail.com>
2014-08-07 09:05:35 +0300

    Introduction factory service api with working left in

--- client/js/cupboardController.js
+++ client/js/cupboardController.js
@@ -1,37 +1,54 @@
 'use strict';
 
- eshoprShop.factory("IngredientFactory", function($resource) {
-    // var factory = {}
-    // factory.getIngredients = function ($resource){
-        var url = "/api/ingredients/:id";
-        return $resource(url);
-    // }
-    // return factory
-
+eshoprShop.factory("IngredientFactory", function($http,$resource) {
+  var factory = {};
+  // factory.getIngredients = function () {
+  //   var url = "/api/ingredients/:id";
+  //   return $resource(url);
+  // };
+      factory.getClients = function () {
+        var url = "/api/ingredients";
+        return $http.get(url);
+    };
+  return factory
 });
+// eshoprShop.service("IngIndexCtrl", function($scope, IngredientFactory) {
+//   Post.query(function(data) {
+//     $scope.posts = data;
+//   });
+// });
 
 // /* Controllers Module for studentDetailApp application*/ 
-var cupboardController = angular.module('cupboardController', ['ui.bootstrap',]);
+var cupboardController = angular.module('cupboardController', ['ui.bootstrap']);
 
-cupboardController.controller('cupboardController', function($scope, $http, IngredientFactory) { 
+cupboardController.controller('cupboardController', function($resource,$scope, $http, IngredientFactory) { 
   // we now must find some way to put ingredients in the 
   $scope.works = 'cupboardController';
+function init() {
+    IngredientFactory.getClients().then(function(response) {
+      console.log(response)
+        $scope.ingredients = response.data;
+    });
+}
+  init();
   // console.log(Ingredient);
   // $scope.listIngredients = function($scope, Ingredient) {
   //   Ingredient.query(function(data) {
   //       $scope.ingredients = data;
   //   });
   // }
-  $scope.ingredients = IngredientFactory.query(function(data) {
-    return data
-  });
-
-  $scope.doStuff = function(id){
-    // $scope.currentitem = id;
-    console.log(id)
-    IngredientFactory.get({id: id}, function(data){
-      $scope.currentitem = data;
-    });
-  }
+  // $scope.ingredients = IngredientFactory.getIngredients().then(function(response) {
+  //   return reponse.data;
+  // });
+// console.log($scope.ingredients);
+  // $scope.doStuff = function(id){
+  //   // $scope.currentitem = id;
+  //   console.log(id)
+  //   var ohm = id;
+  //   IngredientFactory.get({id: 1}, function(data){
+  //     console.log(data)
+  //     $scope.currentitem = data;
+  //   });
+  // }
   // console.log($scope.ingredients);
 });
