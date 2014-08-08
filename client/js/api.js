2559d1ea59ff35d1b025158ee906bf7842b35a27
Andrew <andrewaoidh@gmail.com>
2014-08-09 02:31:03 +0300

    initial outline of controller

--- client/js/api.js
+++ client/js/api.js
@@ -1,9 +1,26 @@
-//  eshoprShop.factory("IngredientFactory", function($resource) {
+'use strict';
 
-//     // factory.getIngredients = function ($resource){
-//         var url = "/api/ingredients/:id";
-//         return $resource(url);
-//     // }
+eshoprShop.factory("IngredientFactory", function($http,$resource) {
+  var factory = {};
+  var url = "/api/ingredients";
 
-//     // return factory;
-// });
+  factory.getIngredients = function () {
+    return $http.get(url);
+  };
+  factory.showIngredient = function (id) {
+    return $http.get(url + '/' + id);
+  };
+  factory.insertIngredient = function (dataObject) {
+    console.log(dataObject);
+    return $http.post(url, dataObject);
+  };
+  // factory.updateIngredient = function (id, dataObject) {
+  //   return $http.put(url + '/' + cust.ID, cust)
+  // };
+
+  factory.deleteIngredient = function (id) {
+        return $http.delete(url + '/' + id);
+  };
+  return factory
+});
+
--- client/js/cupboardController.js
+++ client/js/cupboardController.js
@@ -1,29 +1,3 @@
-'use strict';
-
-eshoprShop.factory("IngredientFactory", function($http,$resource) {
-  var factory = {};
-  var url = "/api/ingredients";
-
-  factory.getIngredients = function () {
-    return $http.get(url);
-  };
-  factory.showIngredient = function (id) {
-    return $http.get(url + '/' + id);
-  };
-  factory.insertIngredient = function (dataObject) {
-    console.log(dataObject);
-    return $http.post(url, dataObject);
-  };
-  // factory.updateIngredient = function (id, dataObject) {
-  //   return $http.put(url + '/' + cust.ID, cust)
-  // };
-
-  factory.deleteIngredient = function (id) {
-        return $http.delete(url + '/' + id);
-  };
-  return factory
-});
-
 var cupboardController = angular.module('cupboardController', ['ui.bootstrap']);
 
 cupboardController.controller('cupboardController', function($resource,$scope, $http, IngredientFactory) { 
