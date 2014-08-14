b4c92d149518b96ed41a045783c993068f0e18d7
Andrew <andrewaoidh@gmail.com>
2014-08-14 11:36:29 +0300

    create recipe functionality added

--- client/js/api.js
+++ client/js/api.js
@@ -28,22 +28,22 @@ eshoprShop.factory("RecipeFactory", function($http,$resource) {
   var factory = {};
   var url = "/api/recipes";
 
-  // factory.getRecipes = function () {
-  //   return $http.get(url);
-  // };
-  // factory.showRecipe = function (id) {
-  //   return $http.get(url + '/' + id);
-  // };
-  // factory.insertRecipe = function (dataObject) {
-  //   console.log(dataObject);
-  //   return $http.post(url, dataObject);
-  // };
-  // factory.updateIngredient = function (id, dataObject) {
-  //   return $http.put(url + '/' + cust.ID, cust)
-  // };
+  factory.getRecipes = function () {
+    return $http.get(url);
+  };
+  factory.showRecipe = function (id) {
+    return $http.get(url + '/' + id);
+  };
+  factory.insertRecipe = function (dataObject) {
+    console.log(dataObject);
+    return $http.post(url, dataObject);
+  };
+  factory.updateIngredient = function (id, dataObject) {
+    return $http.put(url + '/' + cust.ID, cust)
+  };
 
-  // factory.deleteIngredient = function (id) {
-  //       return $http.delete(url + '/' + id);
-  // };
+  factory.deleteIngredient = function (id) {
+        return $http.delete(url + '/' + id);
+  };
   return factory
 });
